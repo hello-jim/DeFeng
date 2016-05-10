51e9b233a54f100a2633fbc29e1aa673a48b6c0e
LIZIDEEP <540948293@qq.com>
2016-05-10 10:27:34 +0800

    。。

---
+++ DeFeng/DeFeng.Common/MD5.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeFeng.Common
+{
+    class MD5
+    {
+        // 加密字符串
+        public string EncryptString(string sInputString, string sKey)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(sInputString);
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+            DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+            ICryptoTransform desencrypt = DES.CreateEncryptor();
+            byte[] result = desencrypt.TransformFinalBlock(data, 0, data.Length);
+            return BitConverter.ToString(result);
+        }
+        // 解密字符串
+        public string DecryptString(string sInputString, string sKey)
+        {
+            string[] sInput = sInputString.Split("-".ToCharArray());
+            byte[] data = new byte[sInput.Length];
+            for (int i = 0; i < sInput.Length; i++)
+            {
+                data[i] = byte.Parse(sInput[i], NumberStyles.HexNumber);
+            }
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+            DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+            ICryptoTransform desencrypt = DES.CreateDecryptor();
+            byte[] result = desencrypt.TransformFinalBlock(data, 0, data.Length);
+            return Encoding.UTF8.GetString(result);
+        }
+
+    }
+}
+
