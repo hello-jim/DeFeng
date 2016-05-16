e63f317a2e6f17ab25aed55eaab6771489d85d4a
hello-jim <842736598@qq.com>
2016-05-16 18:06:17 +0800

    ...

---
+++ DeFeng/DeFeng.Model/DrawMoney.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeFeng.Model
+{
+    public class DrawMoney
+    {
+        private int id;
+        private Department drawMoneyDepartment;//领款部门 
+        private string moneyUse;//用途
+        private decimal money;//金额
+        private string mark;//送审标识
+        private Staff createStaff;
+        private DateTime createDate;
+        private DateTime lastUpdateDate;
+        private Staff lastUpdateStaff;
+
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public Department DrawMoneyDepartment
+        {
+            get
+            {
+                return drawMoneyDepartment;
+            }
+
+            set
+            {
+                drawMoneyDepartment = value;
+            }
+        }
+
+        public string MoneyUse
+        {
+            get
+            {
+                return moneyUse;
+            }
+
+            set
+            {
+                moneyUse = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get
+            {
+                return money;
+            }
+
+            set
+            {
+                money = value;
+            }
+        }
+
+        public string Mark
+        {
+            get
+            {
+                return mark;
+            }
+
+            set
+            {
+                mark = value;
+            }
+        }
+
+        public Staff CreateStaff
+        {
+            get
+            {
+                return createStaff;
+            }
+
+            set
+            {
+                createStaff = value;
+            }
+        }
+
+        public DateTime CreateDate
+        {
+            get
+            {
+                return createDate;
+            }
+
+            set
+            {
+                createDate = value;
+            }
+        }
+
+        public DateTime LastUpdateDate
+        {
+            get
+            {
+                return lastUpdateDate;
+            }
+
+            set
+            {
+                lastUpdateDate = value;
+            }
+        }
+
+        public Staff LastUpdateStaff
+        {
+            get
+            {
+                return lastUpdateStaff;
+            }
+
+            set
+            {
+                lastUpdateStaff = value;
+            }
+        }
+    }
+}
--- DeFeng/DeFeng.Model/SecondHandHouseEarnest.cs
+++ DeFeng/DeFeng.Model/SecondHandHouseEarnest.cs
@@ -8,6 +8,215 @@ namespace DeFeng.Model
 {
     public class SecondHandHouseEarnest
     {
+        private int id;
+        private string shop;//门店
+        private string project;//项目
+        private string houseBuilding;//房栋
+        private string unit;//单元
+        private string houseNo;//房号
+        private string customerName;//客户姓名(买房)
+        private decimal earnest;//定金
+        private DateTime payEarnestDate;//交定日期
+        private string mark;//送审标识
+        private string attachmentName;//附件
+        private Staff createStaff;
+        private DateTime createDate;
+        private DateTime lastUpdateDate;
+        private Staff lastUpdateStaff;
 
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Shop
+        {
+            get
+            {
+                return shop;
+            }
+
+            set
+            {
+                shop = value;
+            }
+        }
+
+        public string Project
+        {
+            get
+            {
+                return project;
+            }
+
+            set
+            {
+                project = value;
+            }
+        }
+
+        public string HouseBuilding
+        {
+            get
+            {
+                return houseBuilding;
+            }
+
+            set
+            {
+                houseBuilding = value;
+            }
+        }
+
+        public string Unit
+        {
+            get
+            {
+                return unit;
+            }
+
+            set
+            {
+                unit = value;
+            }
+        }
+
+        public string HouseNo
+        {
+            get
+            {
+                return houseNo;
+            }
+
+            set
+            {
+                houseNo = value;
+            }
+        }
+
+        public string CustomerName
+        {
+            get
+            {
+                return customerName;
+            }
+
+            set
+            {
+                customerName = value;
+            }
+        }
+
+        public decimal Earnest
+        {
+            get
+            {
+                return earnest;
+            }
+
+            set
+            {
+                earnest = value;
+            }
+        }
+
+        public DateTime PayEarnestDate
+        {
+            get
+            {
+                return payEarnestDate;
+            }
+
+            set
+            {
+                payEarnestDate = value;
+            }
+        }
+
+        public string Mark
+        {
+            get
+            {
+                return mark;
+            }
+
+            set
+            {
+                mark = value;
+            }
+        }
+
+        public string AttachmentName
+        {
+            get
+            {
+                return attachmentName;
+            }
+
+            set
+            {
+                attachmentName = value;
+            }
+        }
+
+        public Staff CreateStaff
+        {
+            get
+            {
+                return createStaff;
+            }
+
+            set
+            {
+                createStaff = value;
+            }
+        }
+
+        public DateTime CreateDate
+        {
+            get
+            {
+                return createDate;
+            }
+
+            set
+            {
+                createDate = value;
+            }
+        }
+
+        public DateTime LastUpdateDate
+        {
+            get
+            {
+                return lastUpdateDate;
+            }
+
+            set
+            {
+                lastUpdateDate = value;
+            }
+        }
+
+        public Staff LastUpdateStaff
+        {
+            get
+            {
+                return lastUpdateStaff;
+            }
+
+            set
+            {
+                lastUpdateStaff = value;
+            }
+        }
     }
 }
